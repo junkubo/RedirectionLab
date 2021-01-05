53513fa252e0c80885f7c98b92ee48e01cd21c85
Jun <jkubo20@stuy.edu>
2021-01-04 19:45:29 -0500

    fix pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -1,12 +1,22 @@
 public class PigLatin {
   public static String pigLatinSimple(String s) {
     String newstr = "";
+    boolean isSpecial = false;
+    if (s.substring(0,1).equals("a") || s.substring(0,1).equals("e") || s.substring(0,1).equals("i") || s.substring(0,1).equals("o") || s.substring(0,1).equals("u")) {
+      isSpecial = true;
+        }
     for (int i = 0; i < s.length(); i++) {
       if (i > 0) {
-        newstr += s[i];
+        newstr += s.substring(i, i + 1);
       }
     }
-    newstr += s[0];
-    newstr += "ay";
+    newstr += s.substring(0, 1);
+    if (isSpecial == true) {
+      newstr += "hay";
+    }
+    else {
+      newstr += "ay";
+    }
+    return newstr;
   }
 }
---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester {
+  public static void main(String[] args) {
+    PigLatin pig = new PigLatin();
+    System.out.println(pig.pigLatinSimple("david"));
+    System.out.println(pig.pigLatinSimple("aaron"));
+  }
+}
