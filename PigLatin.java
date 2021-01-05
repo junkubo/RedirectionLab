bbb0e9a707e2f409a44bda51c6cd7f0fa9fe54f4
Jun <jkubo20@stuy.edu>
2021-01-04 20:15:26 -0500

    fixed pigLatin

--- PigLatin.java
+++ PigLatin.java
@@ -19,4 +19,37 @@ public class PigLatin {
     }
     return newstr;
   }
+  public static String pigLatin(String s) {
+    String newstr = "";
+    boolean isSpecial = false;
+    if (s.substring(0,1).equals("a") || s.substring(0,1).equals("e") || s.substring(0,1).equals("i") || s.substring(0,1).equals("o") || s.substring(0,1).equals("u")) {
+      isSpecial = true;
+        }
+    boolean digraph = false;
+    String[] digraphs = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    for (int i = 0; i < digraphs.length; i++) {
+      if (s.substring(0,2).equals(digraphs[i])) {
+        digraph = true;
+      }
+    }
+    if (digraph) {
+        newstr += s.substring(2);
+        newstr += s.substring(0, 2);
+    }
+    else {
+      for (int i = 0; i < s.length(); i++) {
+      if (i > 0) {
+        newstr += s.substring(i, i + 1);
+      }
+    }
+    newstr += s.substring(0, 1);
+  }
+    if (isSpecial == true) {
+      newstr += "hay";
+    }
+    else {
+      newstr += "ay";
+    }
+    return newstr;
+  }
 }
--- Tester.java
+++ Tester.java
@@ -3,5 +3,11 @@ public class Tester {
     PigLatin pig = new PigLatin();
     System.out.println(pig.pigLatinSimple("david"));
     System.out.println(pig.pigLatinSimple("aaron"));
+    System.out.println(pig.pigLatinSimple("End simple === start pigLatin ==="));
+    System.out.println(pig.pigLatin("the"));
+    System.out.println(pig.pigLatin("check"));
+    System.out.println(pig.pigLatin("skee"));
+    System.out.println(pig.pigLatin("emu"));
+    System.out.println(pig.pigLatin("grade"));
   }
 }
