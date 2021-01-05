6d6548d65a855d3dbd65b20474bd9453979958fc
Jun <jkubo20@stuy.edu>
2021-01-04 19:18:26 -0500

    MakeStars finished

--- MakeStars.java
+++ MakeStars.java
@@ -1,3 +1,28 @@
-public static String makeStars(String s) {
-  
+import java.util.Scanner;
+public class MakeStars {
+  public static void main(String args[]) {
+    Scanner s = new Scanner(System.in);
+    while (s.hasNextLine()) {
+      String str = s.nextLine();
+      String newstr = "";
+      boolean whitespace = false;
+      for (int i = 0; i < str.length(); i++) {
+        if (str.charAt(i) == ' ' || str.charAt(i) == '\n' || str.charAt(i) == '\t' || str.charAt(i) == '\r'){
+          if (!whitespace) {
+            newstr += " ";
+            whitespace = true;
+          }
+          else {}
+
+        }
+        else {
+          newstr += "*";
+          whitespace = false;
+        }
+      }
+      System.out.println(newstr);
+    }
+
+  }
+
 }
--- PigLatin.java
+++ PigLatin.java
@@ -1,3 +1,12 @@
-public static String pigLatinSimple(String s) {
-  
+public class PigLatin {
+  public static String pigLatinSimple(String s) {
+    String newstr = "";
+    for (int i = 0; i < s.length(); i++) {
+      if (i > 0) {
+        newstr += s[i];
+      }
+    }
+    newstr += s[0];
+    newstr += "ay";
+  }
 }
